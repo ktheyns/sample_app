ba0679d0f53f496ab87389cd13af81a851c5d1d8
Kirsten Heyns <kirsten.heyns@gmail.com>
2012-01-13 12:07:39 +0200

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,19 @@
 class PagesController < ApplicationController
   def home
+	  @title = "Home"
   end
 
   def contact
+	  @title = "Contact"
   end
 
   def about
-  end  
+	  @title = "About"
+  end
+
+  def help
+	  @title = "Help"
+  end
+
 
 end
